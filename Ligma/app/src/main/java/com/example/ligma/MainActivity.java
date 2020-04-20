f11bd8e8eb4794fc56b49f2aa8f6da14daecb5ce
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2020-04-20 14:17:49 +0200

    Start spil is made

--- Ligma/app/src/main/java/com/example/ligma/MainActivity.java
+++ Ligma/app/src/main/java/com/example/ligma/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.ligma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClickStartGame(View view){
+        Intent playerSelectionIntent = new Intent(this, PlayerSelection.class);
+        startActivity(playerSelectionIntent);
+    }
+
+    public void onClickRules(View view){
+        Intent rulesIntent = new Intent(this, Rules.class);
+        startActivity(rulesIntent);
     }
 }
