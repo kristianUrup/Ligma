1418a5d5e2bdcbc28e091de6814ffe2fa3e279d0
milimolo <bondemand@hotmail.dk>
2020-04-22 18:45:57 +0200

    Sending list of players and errorhandling
    When starting game, the list of players is sent with it via intent.
    Errorhandling in the form of names having to be filled out and cannot start a game with no players.

--- Ligma/app/src/main/java/com/example/ligma/PlayerSelection.java
+++ Ligma/app/src/main/java/com/example/ligma/PlayerSelection.java
@@ -3,15 +3,17 @@ package com.example.ligma;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class PlayerSelection extends Activity {
     ListView listView;
@@ -19,41 +21,75 @@ public class PlayerSelection extends Activity {
     Button addBtn;
     EditText editText;
     ArrayAdapter<String> arrayAdapter;
+    TextView errorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_selection);
 
-        String[] friends;
         listView = findViewById(R.id.LviewPlayers);
         addBtn = findViewById(R.id.btnAddNewPlayer);
         editText = findViewById(R.id.txtNewPlayer);
+        errorText = findViewById(R.id.txtViewErrorText);
 
         list = new ArrayList<String>();
         arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
                 android.R.layout.simple_list_item_1, list);
 
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                String names = editText.getText().toString();
 
-                list.add(names);
-                listView.setAdapter(arrayAdapter);
-                arrayAdapter.notifyDataSetChanged();
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkAndAddPlayer();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkAndAddPlayer();
             }
         });
-
-
     }
 
     public void onClickAddPlayer(View view){
 
     }
 
+    private void checkAndAddPlayer(){
+        if(!TextUtils.isEmpty(editText.getText().toString())){
+            addBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    if(!TextUtils.isEmpty(editText.getText().toString())){
+                        addToListView();
+                    }
+                }
+            });
+        } else {
+            String noNameErrorText = "You must fill out a name before adding them.";
+            editText.setError(noNameErrorText);
+        }
+    }
+
+    private void addToListView(){
+        String names = editText.getText().toString();
+
+        list.add(names);
+        listView.setAdapter(arrayAdapter);
+        arrayAdapter.notifyDataSetChanged();
+
+        errorText.setText("");
+    }
+
     public void onClickStart(View view){
-        Intent startGameIntent = new Intent(this, TheGame.class);
-        startActivity(startGameIntent);
+        if(list.isEmpty()){
+            String startGameWithoutPlayers = "You cannot start a game with no players";
+            errorText.setText(startGameWithoutPlayers);
+        }else {
+            Intent startGameIntent = new Intent(this, TheGame.class);
+            startGameIntent.putStringArrayListExtra("player_list", list);
+            startActivity(startGameIntent);
+        }
     }
 }
--- Ligma/app/src/main/java/com/example/ligma/TheGame.java
+++ Ligma/app/src/main/java/com/example/ligma/TheGame.java
@@ -2,13 +2,22 @@ package com.example.ligma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class TheGame extends AppCompatActivity {
 
+    ArrayList<String> playerList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_game);
+
+        Intent intent = getIntent();
+        playerList = intent.getStringArrayListExtra("player_list");
+        Log.d("CREATION", "player list: " + playerList.toString());
     }
 }
