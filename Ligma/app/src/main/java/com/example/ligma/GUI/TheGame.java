30c7318724cffd02447461e925af12a8bd1c5c45
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2020-04-22 23:02:34 +0200

    The game view is made

--- Ligma/app/src/main/java/com/example/ligma/GUI/TheGame.java
+++ Ligma/app/src/main/java/com/example/ligma/GUI/TheGame.java
@@ -3,14 +3,34 @@ package com.example.ligma.GUI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.ligma.R;
 
 public class TheGame extends AppCompatActivity {
 
+    TextView cardDesc;
+    TextView playerName;
+    LinearLayout inventory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_game);
+
+        cardDesc = findViewById(R.id.card_description);
+        playerName = findViewById(R.id.player_name);
+        inventory = findViewById(R.id.inventory_layout);
+
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        cardDesc.setText("hello officer, drink 2 shots, but Frederik 400 shots");
+        playerName.setText("Mathias");
+        TextView tvToInsert = new TextView(this);
+        tvToInsert.setLayoutParams(lparams);
+        tvToInsert.setText("TOILET MANNER");
+        this.inventory.addView(tvToInsert);
     }
 }
