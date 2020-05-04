07f6fd9aa8dd2b7ac35143834fe45b878fbde4b0
milimolo <bondemand@hotmail.dk>
2020-05-04 10:30:32 +0200

    Show cards from database.
    No longer needs a mock card to work.

---
+++ Ligma/app/src/main/java/com/example/ligma/DAL/CardDAO.java
@@ -0,0 +1,48 @@
+package com.example.ligma.DAL;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.ligma.BE.Card;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class CardDAO {
+
+    FirebaseFirestore db;
+    public static final String TAG = "TAG";
+
+    public CardDAO(){
+        db = FirebaseFirestore.getInstance();
+    }
+
+    public ArrayList<Card> readCards(FirestoreCallback callback){
+        ArrayList<Card> deckToShuffle = new ArrayList<>();
+
+        db.collection("cards")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            ArrayList<Card> deckToShuffle = new ArrayList<>();
+                            for(QueryDocumentSnapshot document : task.getResult()){
+                                deckToShuffle.add(document.toObject(Card.class));
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                            callback.onCallBack(deckToShuffle);
+                        } else {
+                            Log.w(TAG, "Reading documents resulted in error.", task.getException());
+
+                        }
+                    }
+                });
+        return deckToShuffle;
+    }
+}
