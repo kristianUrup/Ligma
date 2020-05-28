aaddabc23d3aedcb0780685a0f97c20949918607
milimolo <bondemand@hotmail.dk>
2020-05-28 14:25:12 +0200

    CardDao, firestoreCallBack, mainActivity and rules documented

--- Ligma/app/src/main/java/com/example/ligma/GUI/MainActivity.java
+++ Ligma/app/src/main/java/com/example/ligma/GUI/MainActivity.java
@@ -11,18 +11,29 @@ import com.example.ligma.R;
 public class MainActivity extends AppCompatActivity {
 
 
-
+    /**
+     * Sets the view to main activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Sets the view to playerSelection activity.
+     * @param view
+     */
     public void onClickStartGame(View view){
         Intent playerSelectionIntent = new Intent(this, PlayerSelection.class);
         startActivity(playerSelectionIntent);
     }
 
+    /**
+     * Sets the view to rules activity.
+     * @param view
+     */
     public void onClickRules(View view){
         Intent rulesIntent = new Intent(this, Rules.class);
         startActivity(rulesIntent);
--- Ligma/app/src/main/java/com/example/ligma/GUI/Rules.java
+++ Ligma/app/src/main/java/com/example/ligma/GUI/Rules.java
@@ -7,6 +7,10 @@ import com.example.ligma.R;
 
 public class Rules extends Activity {
 
+    /**
+     * Sets the view as the rule activity, showcasing the rules.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
